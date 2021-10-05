06064a743cbcacbea5b2a5fefc5ff7f5412a5357
Mykhailo Gerasymenko <phelpsmisha@gmail.com>
2021-10-05 18:44:41 +0300

    Edit midules

--- gke cluster/main.tf
+++ gke cluster/main.tf
@@ -1,43 +1,3 @@
-provider "google" {
-  version = "~> 3.42.0"
-}
-module "gke_auth" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  depends_on   = [module.gke]
-  project_id   = var.project_id
-  location     = module.gke.location
-  cluster_name = module.gke.name
-}
-resource "local_file" "kubeconfig" {
-  content  = module.gke_auth.kubeconfig_raw
-  filename = "kubeconfig-${var.env_name}"
-}
-module "gcp-network" {
-  source       = "terraform-google-modules/network/google"
-  version      = "~> 2.5"
-  project_id   = var.project_id
-  network_name = "${var.network}-${var.env_name}"
-  subnets = [
-    {
-      subnet_name   = "${var.subnetwork}-${var.env_name}"
-      subnet_ip     = "10.10.0.0/16"
-      subnet_region = var.region
-    },
-  ]
-  secondary_ranges = {
-    "${var.subnetwork}-${var.env_name}" = [
-      {
-        range_name    = var.ip_range_pods_name
-        ip_cidr_range = "10.20.0.0/16"
-      },
-      {
-        range_name    = var.ip_range_services_name
-        ip_cidr_range = "10.30.0.0/16"
-      },
-    ]
-  }
-}
-
 module "gke" {
   source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
   project_id             = var.project_id
@@ -58,4 +18,8 @@ module "gke" {
       disk_size_gb              = 30
     },
   ]
+}
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig-${var.env_name}"
 }
--- network/main.tf
+++ network/main.tf
@@ -1,17 +1,3 @@
-provider "google" {
-  version = "~> 3.42.0"
-}
-module "gke_auth" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  depends_on   = [module.gke]
-  project_id   = var.project_id
-  location     = module.gke.location
-  cluster_name = module.gke.name
-}
-resource "local_file" "kubeconfig" {
-  content  = module.gke_auth.kubeconfig_raw
-  filename = "kubeconfig-${var.env_name}"
-}
 module "gcp-network" {
   source       = "terraform-google-modules/network/google"
   version      = "~> 2.5"
@@ -36,26 +22,4 @@ module "gcp-network" {
       },
     ]
   }
-}
-
-module "gke" {
-  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  project_id             = var.project_id
-  name                   = "${var.cluster_name}-${var.env_name}"
-  regional               = true
-  region                 = var.region
-  network                = module.gcp-network.network_name
-  subnetwork             = module.gcp-network.subnets_names[0]
-  ip_range_pods          = var.ip_range_pods_name
-  ip_range_services      = var.ip_range_services_name
-  node_pools = [
-    {
-      name                      = "node-pool"
-      machine_type              = "e2-medium"
-      node_locations            = "europe-west1-b,europe-west1-c,europe-west1-d"
-      min_count                 = 1
-      max_count                 = 2
-      disk_size_gb              = 30
-    },
-  ]
 }
