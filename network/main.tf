40ce4f80e8331a11bd0ab76828e67bdbb38a4c2a
Mykhailo Gerasymenko <phelpsmisha@gmail.com>
2021-10-07 14:32:15 +0300

    Fix network file

--- network/main.tf
+++ network/main.tf
@@ -1,16 +1,14 @@
-resource "google_compute_network" "vpc_network" {
-  name = var.network
-  project_id   = var.project_id
-  auto_create_subnetworks = false
-}
-
 resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
   name          = var.subnetwork
   ip_cidr_range = "10.10.0.0/16"
   region        = var.region
-  network       = google_compute_network.vpc_networkt.id
+  network       = google_compute_network.custom-test.id
   secondary_ip_range {
     range_name    = var.ip_range_pods_name
     ip_cidr_range = "10.20.0.0/16"
   }
+}
+resource "google_compute_network" "custom-test" {
+  name                    = var.network
+  auto_create_subnetworks = false
 }
